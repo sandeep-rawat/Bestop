2bd7275a7c9454ddf1ae2359d907e365c59a663f
sandeep-rawat <sandeep.rawat@gmail.com>
2015-08-28 00:35:50 -0700

    aboutUs page changes.

--- app/components/aboutUs.jsx
+++ app/components/aboutUs.jsx
@@ -6,28 +6,31 @@ module.exports = React.createClass({
       <div className="container about-us">
         <div className="section vision">
         	<div className="section-header">
-	        	<h3>Vision:</h3>
-	        	<h4>To be a reputed technology organization that bridges gap between businesses’ priorities and technology solutions</h4>
+	        	<h2>Vision:</h2>
+	        	<h3>To be a reputed technology organization that bridges gap between businesses’ priorities and technology solutions</h3>
 	        </div>
+          <img src="../img/vision.jpg" alt="" className="img-responsive"/>
         </div>
         <div className="section mission">
         	<div className="section-header">
-	        	<h3>Mission:</h3>
-	       		<h4>To accomplish the vision with an exclusive focus on Digital technologies</h4>
-        	</div>
+	        	<h2>Mission:</h2>
+	       		<h3>To accomplish the vision with an exclusive focus on Digital technologies</h3>
+          </div>
+          <img src="../img/mission.jpg" alt="" className="img-responsive"/>
         </div>
         <div className="section values">
         	<div className="section-header">
-	        	<h3>Values:</h3>
-						<h4>
+	        	<h2>Values:</h2>
+						<h3>
 							<ul>
 								<li>Absolute Integrity</li>
 								<li>Servant Leadership</li>
 								<li>Hawk-eyed Customer Focus</li>
 								<li>Responsible Global Citizenship</li>
 							</ul>	
-						</h4>	
-					</div>	
+						</h3>
+          </div>	
+          <img src="../img/values.jpg" alt="" className="img-responsive"/>
         </div>
       </div>
     );
